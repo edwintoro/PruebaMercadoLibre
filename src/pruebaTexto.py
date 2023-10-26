6c09f9aca77b7ab19cdb772a26b5012fb44edc66
edwindistoro <edwindistoro@hotmail.com>
2023-10-26 23:48:13 +0000

    se sube jwt

--- src/pruebaTexto.py
+++ src/pruebaTexto.py
@@ -1,30 +1,38 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, jsonify
 import requests
 import database as db
 import datetime
 
 
 
+
 app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "super-secret" 
+from flask_jwt import jwt, jwt_required, current_identity, JWTManager, create_access_token
+jwt = JWTManager(app)
+
 
 @app.route("/login/<params>/<param>" )
 def login(params,param):
 
     print(params,param)
     cursor = db.database.cursor()
-    query = ("SELECT nombreUsuario FROM pruebamercadolibre.usuario WHERE nombreUsuario = %s ")
-    cursor.execute(query, (params))
-    cursor.fetchone()
+    query = ("SELECT nombreUsuario FROM pruebamercadolibre.usuario WHERE nombreUsuario = %s  AND password = %s ")
+    tuple1 = (params, param)
+    print(tuple1)
+    cursor.execute(query,tuple1)
     row = cursor.fetchone()
     if row  == None:
-     return None
+     return ("not")
     else:
         print(row)
-        return redirect(url_for("/members/<params>"))
-
+        token = create_access_token(identity=row)
+        return jsonify(token=token)
 
 
 @app.route("/members/<params>")
+@jwt_required()
 def members(params):
     url = 'https://restcountries.com/v3.1/name/'+ params +'?fullText=true'
     col = requests.get(url)
